2617b4fe145f939ad3a2671a0efeebc113f12c79
saneenKP <saneenkp2000@gmail.com>
2022-03-14 00:28:42 +0530

    log.js corrected with correct nodejs revershell payload

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-const express = require('express');
-const cookieParser = require('cookie-parser')
-const escape = require('escape-html');
-const serialize = require('node-serialize');
-
-var app = express()
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var escape = require('escape-html');
+var serialize = require('node-serialize');
+var app = express();
 app.use(cookieParser())
-
-app.get('/' , (req , res) => {
-
-    if(req.cookies.profile){
-        var str = new Buffer.from(req.cookies.profile , 'base64')
-        var obj = serialize.unserialize(str);
-
-        if(obj.username){
-            res.send("Hello " + escape(obj.username))
-        }
-    }else{
-        res.cookie('profile' , "eyJ1c2VybmFtZSI6IlNhbmVlbiIsImNvdW50cnkiOiJpbmRpYSIsImNpdHkiOiJFcm5ha3VsYW0ifQ==" , {
-            maxAge: 900000,
-            httpOnly: true
-        })
-    }
-
-    res.send("Hello world");
-})
-app.listen(3000)
+ 
+app.get('/', function(req, res) {
+ if (req.cookies.profile) {
+   var str = new Buffer(req.cookies.profile, 'base64').toString();
+   var obj = serialize.unserialize(str);
+   if (obj.username) {
+     res.send("Hello " + escape(obj.username));
+   }
+ } else {
+     res.cookie('profile', "eyJ1c2VybmFtZSI6ImFqaW4iLCJjb3VudHJ5IjoiaW5kaWEiLCJjaXR5IjoiYmFuZ2Fsb3JlIn0=", {
+       maxAge: 900000,
+       httpOnly: true
+     });
+ }
+ res.send("Hello World");
+});
+app.listen(3000);
