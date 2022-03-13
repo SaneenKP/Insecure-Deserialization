f9402d9b14c957a6bdd73980b22ea464f22f6ed0
saneenKP <saneenkp2000@gmail.com>
2022-03-13 16:19:38 +0530

    server completed

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.get('/' , (req , res) => {
 
     if(req.cookies.profile){
         var str = new Buffer(req.cookies.profile , 'base64')
+        var obj = serialize.unserialize(str);
+
+        if(obj.username){
+            res.send("Hello " + escape(obj.username))
+        }
+    }else{
+        res.cookie('profile' , "saneen" , {
+            maxAge: 900000,
+            httpOnly: true
+        })
     }
 
+    res.send("Hello world");
 })
+app.listen(3000)
