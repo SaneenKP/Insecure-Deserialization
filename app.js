3fecd2de9e8781157980bb6f7b1854041cb202d7
saneenKP <saneenkp2000@gmail.com>
2022-03-14 01:02:43 +0530

    app.js changed according to medium article

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ app.use(cookieParser())
 app.get('/', function(req, res) {
  if (req.cookies.profile) {
    var str = new Buffer(req.cookies.profile, 'base64').toString();
-   var obj = serialize.unserialize(str);
+   var obj = serialize.unserialize(str , (err) => {
+       if(err){
+           console.log(err)
+       }
+   });
    if (obj.username) {
      res.send("Hello " + escape(obj.username));
    }
  } else {
-     res.cookie('profile', "eyJ1c2VybmFtZSI6ImFqaW4iLCJjb3VudHJ5IjoiaW5kaWEiLCJjaXR5IjoiYmFuZ2Fsb3JlIn0=", {
+     res.cookie('profile', "eyJ1c2VybmFtZSI6IkFkaXR5YSIsImNvdW50cnkiOiJpbmRpYSIsImNpdHkiOiJEZWxoaSJ9", {
        maxAge: 900000,
        httpOnly: true
      });
@@ -21,3 +25,4 @@ app.get('/', function(req, res) {
  res.send("Hello World");
 });
 app.listen(3000);
+console.log("Listening on port 3000...");
